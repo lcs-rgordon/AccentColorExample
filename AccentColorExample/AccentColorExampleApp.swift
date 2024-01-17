a81148684054f3c659e30f9b3a2a19fc08b9b040
Russell Gordon <rgordon@lcs.on.ca>
2024-01-17 16:12:46 -0500

    Initial Commit

---
+++ AccentColorExample/AccentColorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccentColorExampleApp.swift
+//  AccentColorExample
+//
+//  Created by Russell Gordon on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct AccentColorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
