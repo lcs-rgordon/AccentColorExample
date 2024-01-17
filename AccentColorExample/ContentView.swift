6f0f859fe1bdf977a119043407fbc51dc3275b20
Russell Gordon <rgordon@lcs.on.ca>
2024-01-17 16:30:37 -0500

    Examples of using accent color explicltly as a foreground style.

--- AccentColorExample/ContentView.swift
+++ AccentColorExample/ContentView.swift
@@ -10,10 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // AccentColor in the asset catalog and
+            // can be accessed via Color.accent or just
+            // .accent
             Image(systemName: "globe")
-                .imageScale(.large)
+                .resizable()
+                .scaledToFit()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.accent)
+            // Color.tint is an alias for Color.accent
+            // .tint is a shortform alias for .accent
+            Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // We can override the AccentColor using the
+            // view modifier .tint, which is different than
+            // Color.tint! 🤯
+            //
+            // Here we override to Color.purple
+            //
+            // NOTE: We must use .foregroundStyle(.tint) for
+            //       the override to take effect
+            Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.tint)
+                .tint(.purple)
         }
         .padding()
     }
